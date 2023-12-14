58aec9d2cdca1ee5767512f5cc2b6b4c0c8bf6fd
LuisSantos16 <lmsr.1601@gmail.com>
2023-12-14 09:59:15 -0500

    page update 10

--- js/sweetalert.js
+++
@@ -1,8 +0,0 @@
-Swal.fire({
-    title: '<span class="color-form">Bienvenido(a)!</span>',
-    html: '<span class="color-form">¡Hey! Échale un ojo a mi página web. ¡Espero que te guste!</span>',
-    confirmButtonText: '<span class="color-form">Ingresar</span>',
-    background: 'var(--bg-primary)',
-    allowOutsideClick: false,
-    allowEscapeKey: true,
-})
